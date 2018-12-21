7b6e3f62e03b029610c9d02bedec22de8f5b0ef9
Ilya Afanasev <szrnamerg@gmail.com>
2018-12-21 19:31:12 +0400

    Added first file
    This file contains first C# code for the project. Main features:
    1) Starting and finishing the game possibilities.
    2) Choosing and confirming main character's name.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Int32;
+
+
+namespace TrialGame
+{
+    class Program
+    {
+     public static string givingNameToCharacter ()
+        {
+            Console.WriteLine("Type the name of your creature");
+            String str = Console.ReadLine();
+            return str;
+    }
+        public static void preGiving()
+        {
+            String result = givingNameToCharacter();
+            var player = new Creature.YourCreature(result);
+            Console.WriteLine("Your name is {0}. If OK, enter 1, if not, enter 0", player.name);
+        }
+        static void Main(string[] args)
+        {
+            Boolean isNameGiven = false;
+            if (!isNameGiven)
+            {
+                do preGiving(); while (Console.ReadLine() == "0");
+            }
+            
+            
+            Console.WriteLine("For End press Enter");
+            String finish = Console.ReadLine();
+        }
+    }
+    class Creature
+    {
+        
+        public class YourCreature
+        {
+            public string name; 
+            public YourCreature(String givenName)
+            {
+                name = givenName;    
+            }
+        }
+    }
+}
