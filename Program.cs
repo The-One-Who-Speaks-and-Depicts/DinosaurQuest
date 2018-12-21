d15a81223a0395de02bcfcf13be9dc1f2a930002
Ilya Afanasev <szrnamerg@gmail.com>
2018-12-21 23:46:43 +0400

    C# code
    1) The possibility of accidentally choosing the wrong name by starting typing the right one in the stage of rejection is fixed. Now the only way to accept is to type "1".
    2) Units of classes are now accessible throughout the main code, at least the unit that responses for your character, which should be constant.

--- Program.cs
+++ Program.cs
@@ -10,41 +10,53 @@ namespace TrialGame
 {
     class Program
     {
-     public static string givingNameToCharacter ()
+        public static string givingNameToCharacter()
         {
             Console.WriteLine("Type the name of your creature");
             String str = Console.ReadLine();
             return str;
-    }
-        public static void preGiving()
+        }
+        public static string preGiving()
         {
             String result = givingNameToCharacter();
-            var player = new Creature.YourCreature(result);
-            Console.WriteLine("Your name is {0}. If OK, enter 1, if not, enter 0", player.name);
+            var player = new YourCreature(result);
+            Console.WriteLine("Your name is {0}. If OK, enter 1, if not, enter anything else", player.name);
+            return player.name;
         }
         static void Main(string[] args)
         {
+            YourCreature character = new YourCreature();
             Boolean isNameGiven = false;
             if (!isNameGiven)
             {
-                do preGiving(); while (Console.ReadLine() == "0");
+                do 
+                    {
+                    character.name = preGiving();
+                }
+                        while (Console.ReadLine() != "1");
             }
-            
+            Console.WriteLine("Congratulations! You've chosen your name. Now you are called {0}", character.name);
             
             Console.WriteLine("For End press Enter");
             String finish = Console.ReadLine();
         }
     }
-    class Creature
-    {
-        
+    
+
         public class YourCreature
         {
-            public string name; 
+            public string name;
+
+            public YourCreature()
+            {
+
+            } 
+
             public YourCreature(String givenName)
             {
-                name = givenName;    
+                name = givenName;
             }
+
         }
-    }
+    
 }
