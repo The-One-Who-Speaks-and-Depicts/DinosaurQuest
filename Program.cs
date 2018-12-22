2b94888ac54f439487e41895fcc00e531cb9e09f
Ilya Afanasev <szrnamerg@gmail.com>
2018-12-22 12:00:37 +0400

    The same C# file
    1) Strange isNameGiven in the end is deleted
    2) Functions for basic stats are added
    3)  Functions for special stats are being primitively developed
    4) Working out exceptions is started

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace TrialGame
 {
     class Program
     {
+        public static void StatInfo (int option)
+        {
+            Console.WriteLine("Info about stats shallst be here");
+        }
+
+        public static int ChooseSprint (int option)
+        {
+            int sprint;
+            Console.WriteLine("Choose your sprinter now");
+            sprint = Convert.ToInt32(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            return sprint;
+        }
         public static string givingNameToCharacter()
         {
             Console.WriteLine("Type the name of your creature");
@@ -26,16 +39,50 @@ namespace TrialGame
         static void Main(string[] args)
         {
             YourCreature character = new YourCreature();
-            Boolean isNameGiven = false;
-            if (!isNameGiven)
-            {
+            character.name = "персонаж";
+            
                 do 
                     {
                     character.name = preGiving();
                 }
                         while (Console.ReadLine() != "1");
-            }
+            
             Console.WriteLine("Congratulations! You've chosen your name. Now you are called {0}", character.name);
+            character.SetDefault(0,100, 0);
+            Console.WriteLine("Your level is {0}. Your health is {1}. You have {2} experience", character.level, character.health, character.exp);
+            Console.WriteLine("Choose your master skills. You have 30 points to delegate into the following parameters, with minimum of 1 and maximum of 10: sprinter, stayer, intelligence, progressivity, perception, luck. To see more information press 1, to go to choosing stats press 2");
+            ConsoleKeyInfo readKey;
+            readKey = Console.ReadKey();
+            int decision;
+            decision = Convert.ToInt32(readKey.KeyChar);
+            Console.WriteLine();
+            if (decision >= 51)
+            {
+                int errorNumber;
+                try
+                {
+                    errorNumber = decision;
+                }
+                catch (Exception e)
+                {
+                    do {
+                        Console.WriteLine("You have to type 1 or 2");
+                        readKey = Console.ReadKey();
+                        decision = Convert.ToInt32(readKey.KeyChar);
+                    }
+                    while (decision > 51);
+                }
+            }
+                else if (decision == 49)
+                    {
+                    StatInfo(decision);
+                    }
+                else if (decision == 50)
+                {
+                character.sprint = ChooseSprint(decision);
+                }
+        
+
             
             Console.WriteLine("For End press Enter");
             String finish = Console.ReadLine();
@@ -46,6 +93,10 @@ namespace TrialGame
         public class YourCreature
         {
             public string name;
+            public int health;
+            public int level;
+            public int exp;
+            public int sprint;
 
             public YourCreature()
             {
@@ -56,6 +107,13 @@ namespace TrialGame
             {
                 name = givenName;
             }
+
+            public void SetDefault (int level, int health, int exp)
+        {
+            this.level = level;
+            this.health = health;
+            this.exp = exp;
+        }
 
         }
     
