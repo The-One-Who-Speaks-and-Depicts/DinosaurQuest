4ffee0c49f1b044d2cdbcbb285ec648365f0eb92
Ilya Afanasev <szrnamerg@gmail.com>
2019-01-07 12:40:45 +0400

    Level1 is added
    Transition of character is guaranteed.

---
+++ Level1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialGame
+{
+    class Level1
+    {
+
+        public static void Level1_Entrance(YourCreature character)
+        {
+            Program.Exit_Game();
+        }
+
+    }
+}
