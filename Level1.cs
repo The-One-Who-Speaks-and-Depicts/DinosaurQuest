ee67a5213ae692ef4770aa9938520f6c38fb8f56
Ilya <szrnamerg@gmail.com>
2019-03-14 10:33:25 +0400

    First commit

---
+++ Level1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialGame
+{
+    class Level1
+    {
+
+        public static void Level1_Entrance(YourCreature character)
+        {
+            RecordedSaves.Save_Game(character, 2);
+            Tile nest = new Tile("noDir", "autumn", "nest", character);
+            Program.Exit_Game();
+        }
+
+    }
+}
