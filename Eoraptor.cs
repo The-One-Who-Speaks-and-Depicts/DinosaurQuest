6d382935ec6138f50311b94e2efbf51c809adc36
Ilya <szrnamerg@gmail.com>
2020-06-25 22:18:36 +0400

    adaptation perks are being implemented

---
+++ Eoraptor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrialGame
+{
+    class Eoraptor : Perk
+    {
+
+        public Eoraptor()
+        {
+            this.Name = "Eoraptor";
+            this.Desc = "Eoraptors were not exactly intelligent, so they lack more heuristic approaches to hunt, to fight, to help friends and enchant partners. However, their breed usually increases their traits, becomes more effective and accustomed to the world around, and Eoraptors' bodies develop some very unusual traits, although these traits can do bad for them.";
+        }
+
+
+    }
+}
