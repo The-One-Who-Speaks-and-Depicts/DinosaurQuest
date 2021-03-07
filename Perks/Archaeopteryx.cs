6c3ea85bcac667d415cc6d93206fd124bd2e68ff
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-03-08 02:50:17 +0400

    ADD: IPerk implementation for Archaeopteryx | FORMATTING: IPerk odd intervals

--- Perks/Archaeopteryx.cs
+++ Perks/Archaeopteryx.cs
@@ -1,9 +1,13 @@
+using DinosaurQuest.Tiles;
+using DinosaurQuest.Creatures;
+using System;
+
 namespace DinosaurQuest.Perks
 {
-	public class Archaeopteryx//: IPerk
+	public class Archaeopteryx: IPerk
 	{
 		
-		/*public string name 
+		public string name 
 		{
 			get
 			{
@@ -32,8 +36,53 @@ namespace DinosaurQuest.Perks
 			}
 		}
 		public int coolDown {get; set;}
-		bool isGettable = false;
-		bool isAcquired = true;*/
+		public bool isGettable {get; set; } =  false;
+		public bool isAcquired {get; set; } = true;
+
+		public void OnTile(Character character, ITile tile)
+		{
+			throw new NotImplementedException();
+		}
+        public void OnMovement(Character character, ITile departingTile, ITile destinationTile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnPartner(Character character, ICreature partner, ITile tile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnFriend(Character character, ICreature friend, ITile tile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnRival(Character character, ICreature rival, ITile tile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnPrey(Character character, ICreature prey, ITile tile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnEnemy(Character character, ICreature enemy, ITile tile)
+        {
+			throw new NotImplementedException();
+		}
+        public void OnBreeding(Character character, Character partner, Character descendant)
+        {
+			throw new NotImplementedException();
+		}
+        public void CoolDownSet()
+        {
+			throw new NotImplementedException();
+		}
+        public void UnBlock()
+        {
+			throw new NotImplementedException();
+		}
+        public void Refresh()
+        {
+			throw new NotImplementedException();
+		}
 
 	}
 }
--- Perks/IPerk.cs
+++ Perks/IPerk.cs
@@ -21,21 +21,13 @@ namespace DinosaurQuest.Perks
         void OnTile(Character character, ITile tile);
         void OnMovement(Character character, ITile departingTile, ITile destinationTile);
         void OnPartner(Character character, ICreature partner, ITile tile);
-
         void OnFriend(Character character, ICreature friend, ITile tile);
-
         void OnRival(Character character, ICreature rival, ITile tile);
-
         void OnPrey(Character character, ICreature prey, ITile tile);
-
         void OnEnemy(Character character, ICreature enemy, ITile tile);
-
         void OnBreeding(Character character, Character partner, Character descendant);
-
         void CoolDownSet();
-
         void UnBlock();
-
         void Refresh();
     }
 
