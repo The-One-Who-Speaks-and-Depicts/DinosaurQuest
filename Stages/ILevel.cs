528cf58bb7b341b8acd25783e8355a56c14d4047
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-03-07 14:57:42 +0400

    ADD: ILevel interface

---
+++ Stages/ILevel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using DinosaurQuest.Landscapes;
+using DinosaurQuest.Creatures;
+
+
+namespace DinosaurQuest.Stages
+{
+    public interface ILevel
+    {
+        string name { get; set; }
+        string levelDesc { get; set; }
+        int X_length { get; set; }
+        int Y_length { get; set; }
+        int Area 
+        {
+            get 
+            {
+                return X_length * Y_length;
+            }
+        }
+
+
+        List<ICreature> accessibleCreatures { get; set; }
+
+        List<ILandscape> landscapes { get; set; }
+        public Character currentCharacter { get; set; }
+
+        public void Entrance (Character _currentCharacter)
+        {
+            
+        } 
+        
+        public Character Transfer (Character _currentCharacter)
+        {
+            return _currentCharacter;
+        }
+    }
+}
