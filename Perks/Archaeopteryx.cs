908b76c9575cd9b77e9206f93c322b21c5318c57
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-03-07 23:00:21 +0400

    ADD: Archaeopteryx perk class

---
+++ Perks/Archaeopteryx.cs
@@ -0,0 +1,29 @@
+namespace DinosaurQuest.Perks
+{
+	public class Archaeopteryx//: IPerk
+	{
+		/*
+		public string name 
+		{
+			get
+			{
+				return "Archaeopteryx";
+			}
+		}
+		public string desc
+		{
+			get
+			{
+				return "Archaeopteryx description"; // implement
+			}
+		}
+		public int branch
+		{
+			get
+			{
+				return 0;
+			}
+		}*/
+
+	}
+}
