4d98b8e20642a15f1a26167cfb8d7cc71afacade
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-05-16 23:33:03 +0400

    ADD: Objective: name, status, description

--- GameFunctions/Objective.cs
+++ GameFunctions/Objective.cs
@@ -2,6 +2,38 @@ namespace DinosaurQuest.GameFunctions
 {
 	public class Objective
 	{
+		public string name {get ; set;}
+		public string description {get; set;}
+		public enum Status
+		{
+			Hidden = 0,
+			Active,
+			Achieved,
+			Failed
+		}
+		private Status currentStatus {get; set;}
 
+		public Objective (string _name, string _description)
+		{
+			name = _name;
+			description = _description;
+			currentStatus = setStatus(0);
+		}
+
+		public Status setStatus(int _status)
+		{
+			switch (_status) 
+			{
+				case 0:
+					return Status.Hidden;
+				case 2:
+					return Status.Achieved;
+				case 3: 
+					return Status.Failed;
+				case 1:
+				default:
+					return Status.Active;
+			}
+		}
 	}
 }
