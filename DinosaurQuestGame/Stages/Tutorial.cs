3f4b38e43430c487bb3b1eda8fde6967b8860829
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-22 01:48:47 +0400

    ADD: setting status correction

--- DinosaurQuestGame/Stages/Tutorial.cs
+++ DinosaurQuestGame/Stages/Tutorial.cs
@@ -15,13 +15,7 @@ namespace DinosaurQuest.Stages
 	    public int Area => X_length * Y_length;
 	    public List<Type> accessibleCreatures { get; private set; }
 	    public List<ITerritory> territories { get; private set; }
-	    public List<Objective> objectives => new List<Objective>()
-		{
-			new Objective("Stay alive!", "With death of this one the journey of its watcher ends.", true),
-			new Objective("Find something to eat", "Staying alive is possible only when one has enough energy to move. So, one should search for something that moves to eat it", true),
-			new Objective("Guard the nest", "One is caring not only for oneself, but for their children as well. Thought, sometimes exact priorities may shift a bit", false)
-		};
-
+	    public List<Objective> objectives{get; private set;}
 	    public Character currentCharacter { get; private set; }
 
 	    public void Entrance ()
@@ -38,7 +32,20 @@ namespace DinosaurQuest.Stages
 	    public Tutorial(Character _currentCharacter)
 	    {
 	    	Console.WriteLine($"Entering {name}...");
-	    	// generate objectives
+			Objective stayingAlive = new Objective("Stay alive!", "With death of this one the journey of its watcher ends.", true);
+			stayingAlive.SetStatus(1);
+			Objective foodSearch = new Objective("Find something to eat", "Staying alive is possible only when one has enough energy to move. So, one should search for something that moves to eat it", true);
+			foodSearch.SetStatus(1);
+			Objective guard = new Objective("Guard the nest", "One is caring not only for oneself, but for their children as well. Thought, sometimes exact priorities may shift a bit", true);
+			guard.SetStatus(1);
+			objectives = new List<Objective>()
+			{
+				stayingAlive, foodSearch, guard
+			};
+			for(int i = 0; i < objectives.Count; i++)
+			{
+				Console.WriteLine(objectives[i].ToString());
+			}
 	    	// generate accessible creatures
 	    	// generate center tile
 	    	currentCharacter = _currentCharacter;
