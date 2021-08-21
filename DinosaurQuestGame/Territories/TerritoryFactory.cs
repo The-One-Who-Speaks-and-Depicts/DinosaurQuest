712d4c3c67aa4bf498d75b130c18df0da6d0fb6c
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-22 00:51:37 +0400

    ADD: GenerateStart dummy

--- DinosaurQuestGame/Territories/TerritoryFactory.cs
+++ DinosaurQuestGame/Territories/TerritoryFactory.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Reflection;
+using DinosaurQuest.Stages;
 
 namespace DinosaurQuest.Territories
 {
     public static class TerritoryFactory
     {
+        public static ITerritory GenerateStart (ILevel level)
+        {
+            return new Tile();
+        }
+
         public static ITerritory Generate (ITerritory previousTerritory, Type thisTerritoryType)
         {
             return (ITerritory) Activator.CreateInstance(thisTerritoryType);
