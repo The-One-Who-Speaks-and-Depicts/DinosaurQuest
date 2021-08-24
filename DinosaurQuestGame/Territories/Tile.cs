094757d2bd634e24deea06eac06b82da691ec62d
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-24 21:06:14 +0400

    DELETE: unnecessary usings in Tile

--- DinosaurQuestGame/Territories/Tile.cs
+++ DinosaurQuestGame/Territories/Tile.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using DinosaurQuest.Creatures;
 using DinosaurQuest.Stages;
 using DinosaurQuest.Landscapes;
