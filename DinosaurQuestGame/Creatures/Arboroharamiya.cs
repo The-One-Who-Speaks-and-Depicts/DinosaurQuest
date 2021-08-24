e797293a7579050c0c63916f98b0e1fc9ad5807c
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-24 17:35:13 +0400

    ADD: Arborohamiya dummy

---
+++ DinosaurQuestGame/Creatures/Arboroharamiya.cs
@@ -0,0 +1,32 @@
+using DinosaurQuest.Creatures;
+using DinosaurQuest.Territories;
+using System;
+
+namespace DinosaurQuest.Creatures
+{
+    public class Arboroharamiya : ICreature
+    {
+        public string name => "Arboroharamiya";
+        public int health {get; private set;}
+        public int maxHealth => new Random().Next(40, 60);
+        public int stamina {get; private set;}
+        public int maxStamina => new Random().Next(20, 30);
+        public int attackCoefficient => 0;
+        public int defenceCoefficient => 0;
+        public void Move(ITerritory source, ITerritory target, bool directed = false)
+        {
+            source.creatures.Remove(this);
+            if (directed)
+            {
+                target.creatures.Add(this);
+            }
+        }
+
+        public void Frighten (int coefficient)
+        {
+            
+        }
+
+
+    }
+}
