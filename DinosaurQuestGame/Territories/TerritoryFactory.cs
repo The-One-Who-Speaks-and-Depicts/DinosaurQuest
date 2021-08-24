4007a7579ed226550f22900c2af630f04a555bb4
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2021-08-24 20:35:41 +0400

    ADD: joined and implemented start generators

--- DinosaurQuestGame/Territories/TerritoryFactory.cs
+++ DinosaurQuestGame/Territories/TerritoryFactory.cs
@@ -1,20 +1,15 @@
 using System;
 using System.Reflection;
 using DinosaurQuest.Stages;
+using DinosaurQuest.Landscapes;
 
 namespace DinosaurQuest.Territories
 {
     public static class TerritoryFactory
     {
-        public static Tile GenerateTutorialStart()
+        public static ITerritory GenerateStart (ILevel level, int X, int Y, Landscape.Option option)
         {
-            return new Tile();
-        }
-
-
-        public static ITerritory GenerateStart (ILevel level)
-        {
-            return new Tile();
+            return new Tile(level, X, Y, new Landscape(option));
         }
 
         public static ITerritory Generate (ITerritory previousTerritory, Type thisTerritoryType)
