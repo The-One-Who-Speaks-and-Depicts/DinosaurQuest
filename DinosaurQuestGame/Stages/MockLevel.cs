d8c65b4b9f7dafa6f6d2f0222c51cfb66775d5f0
The-One-Who-Speaks-And-Depicts <szrnamerg@gmail.com>
2021-09-30 15:58:56 +0400

    DELETE: Moq reference in MockLevel

--- DinosaurQuestGame/Stages/MockLevel.cs
+++ DinosaurQuestGame/Stages/MockLevel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using DinosaurQuest.Territories;
 using DinosaurQuest.Creatures;
 using DinosaurQuest.GameFunctions;
-using Moq;
 
 namespace DinosaurQuest.Stages
 {
